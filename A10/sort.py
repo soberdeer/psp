dc86b733102e061ca8e48638b3d4caf97effc7bd
soberdeer <rarerial@mail.ru>
2016-09-21 15:40:51 +0300

    2

--- A10/sort.py
+++ A10/sort.py
@@ -1,16 +1,26 @@
-import operator
-
-
 def merge_sort(file):
+    lines_f = []
     lines = []
-    cols = [0, 1]
-    lines_f = [[]]
-    for rec in file:
-        for l, col in lines_f:
-            l.append(float(lines_f))
-    print(lines_f)
+    for line in file:
+        lines_f.append(' '.join(line.split()))
+    for item in lines_f:
+        lines.append(item.split())
+    final_lst = convert(lines)
     file.close()
+    print_table(final_lst)
+    col = input('Please, choose the column by which you want to sort: 1 for Loc, 2 for Loc/Method\n')
+    sort_table(final_lst, int(col)) if col == '1' or col == '2' else print('capital escape!')
 
 
-file = open('/Users/telse/IdeaProjects/psp/1b.txt', 'r+')
-merge_sort(file)
+def convert(lines):
+    return [[float(i) for i in j] for j in lines]
+
+
+def sort_table(table, col):
+    table.sort(key=lambda x: x[col])
+    print_table(table)
+
+
+def print_table(lst):
+    for elem in lst:
+        print(elem)
